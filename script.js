806ae3f34074e9dfe86ddc51fcab8715f73d89eb
Aritra <aritralahiri17@gmail.com>
2023-09-27 21:19:33 +0530

    added document by tagname

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const list = document.getElementsByClassName("list-group-item");
+const listTg = document.getElementsByClassName("list-group-item-c");
 list[2].style.backgroundColor = "green";
 list[2].style.color = "#ccc";
+listTg[0].style.color = "green";
 for (let i = 0; i < list.length; i++) {
   list[i].style.fontWeight = "bold";
 }
