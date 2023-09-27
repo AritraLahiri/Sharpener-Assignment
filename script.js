6ae84951cbcf509a264064b7dcf5999fcb96a13a
Aritra <aritralahiri17@gmail.com>
2023-09-27 21:26:02 +0530

    added document by QuerySelector

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const list = document.getElementsByClassName("list-group-item");
 const listTg = document.getElementsByClassName("list-group-item-c");
+const listQr = document.querySelectorAll(".list-group-item");
+listQr[1].style.color = "green";
 list[2].style.backgroundColor = "green";
 list[2].style.color = "#ccc";
 listTg[0].style.color = "green";
 for (let i = 0; i < list.length; i++) {
   list[i].style.fontWeight = "bold";
+}
+for (let i = 0; i < listQr.length; i++) {
+  if (i % 2 != 0) {
+    listQr[i].style.color = "green";
+  }
 }
