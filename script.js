0dd793bee7e36bfc97f6cca122afe3e972608715
Aritra <aritralahiri17@gmail.com>
2023-09-28 21:30:51 +0530

    added div

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const list = document.getElementsByClassName("list-group-item");
 const listTg = document.getElementsByClassName("list-group-item-c");
 const listQr = document.querySelectorAll(".list-group-item");
+const listHeading = document.querySelectorAll(".title");
 listQr[1].style.color = "green";
 list[2].style.backgroundColor = "green";
 list[2].style.color = "#ccc";
@@ -13,3 +14,15 @@ for (let i = 0; i < listQr.length; i++) {
     listQr[i].style.color = "green";
   }
 }
+let div = document.createElement("div");
+let div2 = document.createElement("div");
+let textNode = document.createTextNode("Hello");
+let textNode2 = document.createTextNode("Hello");
+div.appendChild(textNode);
+div2.appendChild(textNode2);
+let container = document.querySelector("#main");
+let items = document.querySelector("#items");
+let list1 = document.querySelectorAll(".list-group-item")[0];
+let h2 = document.querySelectorAll(".title");
+items.insertBefore(div, list1);
+container.insertBefore(div2, h2[0]);
