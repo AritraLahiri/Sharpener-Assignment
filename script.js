4119a0488b7eb083e48bd7992215c65426091e10
Aritra <aritralahiri17@gmail.com>
2023-09-29 23:23:14 +0530

    add value to localstorage

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 console.log("Hello World !");
 function onSignUp(event) {
   event.preventDefault();
-  console.log(
-    event.target.username.value +
-      " " +
-      event.target.email.value +
-      " " +
-      event.target.date.value +
-      " " +
-      event.target.timeCall.value +
-      " "
-  );
+  localStorage.setItem("Username", event.target.username.value);
+  localStorage.setItem("Email", event.target.email.value);
+  localStorage.setItem("Date", event.target.date.value);
+  localStorage.setItem("Time", event.target.timeCall.value);
 }
