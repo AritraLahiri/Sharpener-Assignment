af0e656574db9e135fadf56b0a625f0b245d62c1
Aritra <aritralahiri17@gmail.com>
2023-09-30 09:45:30 +0530

    scaled up the app

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-console.log("Hello World !");
 function onSignUp(event) {
   event.preventDefault();
-  localStorage.setItem("Username", event.target.username.value);
-  localStorage.setItem("Email", event.target.email.value);
-  localStorage.setItem("Date", event.target.date.value);
-  localStorage.setItem("Time", event.target.timeCall.value);
-  localStorage.setItem("myObj", JSON.stringify(event));
+  const div = document.querySelector("#showUsers");
+  const userObj = {
+    Name: event.target.username.value,
+    Email: event.target.email.value,
+    Date: event.target.date.value,
+    Phone: event.target.phone.value,
+    Time: event.target.timeCall.value,
+  };
+  localStorage.setItem(event.target.email.value, JSON.stringify(userObj));
+  const { Name, Email, Phone } = JSON.parse(
+    localStorage.getItem(event.target.email.value)
+  );
+  const p = document.createElement("p");
+  const data = document.createTextNode(
+    ` Name : ${Name} , Email : ${Email} , Phone ${Phone} `
+  );
+  p.appendChild(data);
+  div.appendChild(p);
 }
