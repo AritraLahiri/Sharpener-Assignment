cb15d706ee7afab22cf25f2b1ca2266c230638db
Aritra <aritralahiri17@gmail.com>
2023-09-30 12:03:15 +0530

    added delete btn

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+const div = document.querySelector("#showUsers");
 function onSignUp(event) {
   event.preventDefault();
-  const div = document.querySelector("#showUsers");
   const userObj = {
     Name: event.target.username.value,
     Email: event.target.email.value,
@@ -13,9 +13,21 @@ function onSignUp(event) {
     localStorage.getItem(event.target.email.value)
   );
   const p = document.createElement("p");
+  const btn = document.createElement("button");
+  btn.id = Email;
   const data = document.createTextNode(
     ` Name : ${Name} , Email : ${Email} , Phone ${Phone} `
   );
+  const btnData = document.createTextNode("Delete");
+  btn.addEventListener("click", function () {
+    if (confirm("Do you want to delete user?")) {
+      localStorage.removeItem(this.id);
+      this.parentElement.removeChild(this.previousSibling);
+      this.parentElement.removeChild(this);
+    }
+  });
   p.appendChild(data);
+  btn.appendChild(btnData);
   div.appendChild(p);
+  div.appendChild(btn);
 }
